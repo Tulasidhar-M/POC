d7088229b159fca2c03ca241b818cf6433ae7120
tulasidhar.mathamsetti <tulasidhar.mathamsetti@techigai.io>
2023-02-27 17:39:15 +0530

    second commit

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -19,7 +19,7 @@ const Dashboard = () => {
         <ul className="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block" aria-labelledby="dropdownMenuButton1" data-te-dropdown-menu-ref>
           <li>
             {data.map((item, key) => {
-              return <button key={key} onClick={()=>setprojectNo(item.id)} className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600" data-te-dropdown-item-ref>{item.name}</button>
+              return <button key={key} onClick={()=>setprojectNo(item.ProjectId)} className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600" data-te-dropdown-item-ref>{item.ProjectName}</button>
             })}
           </li>
         </ul>
